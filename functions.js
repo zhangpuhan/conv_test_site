3b408d942496875833f3a37c25d3f02eb523a605
Puhan Zhang <32048073+zhangpuhan@users.noreply.github.com>
2021-06-23 09:59:45 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -19,7 +19,7 @@ function is_chrome() {
   }
 }
 
-function chrome_measurement_api() {
+function chrome_measurement_api(event_name, current_domain, pixel_id) {
   var chrome_browser = is_chrome();
   console.log(chrome_browser)
   if (chrome_browser) {
@@ -29,9 +29,14 @@ function chrome_measurement_api() {
       FB.api(
         '/aem_conversion_bit_mapping',
         'GET',
-        {'current_domain':'puhanzhang.com','pixel_id':'512586253523202','event_name':'Purchase', 'access_token': access_token},
+        {'current_domain':'puhanzhang.com','pixel_id': pixel_id,'event_name': event_name, 'access_token': access_token},
         function(res) {
           console.log(res);
+          let response_map = new Map(res);
+          if (response_map.has('conversion_bits') && response_map.has('priority')) {
+            console.log(response_map['conversion_bits']);
+            console.log(response_map['priority']);
+          }
         }
       );} 
     console.log(response);});
